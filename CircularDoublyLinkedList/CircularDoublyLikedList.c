52401315815270a77a9dc09be832f46e5545cdcf
HuitaePark <qkrgmlxo3174@gmail.com>
2025-01-19 02:25:39 +0900

    환형링크드리스트 추가

--- CircularDoublyLinkedList/CircularDoublyLikedList.c
+++ CircularDoublyLinkedList/CircularDoublyLikedList.c
@@ -1,7 +1,7 @@
-#include "DoublyLinkedList.h"
+#include "CircularDoublyLinkedList.h"
 
 //노드 생성
-Node* DLL_CreateNode(ElementType NewData){
+Node* CDLL_CreateNode(ElementType NewData){
     Node* NewNode = (Node*)malloc(sizeof(Node);
 
     NewNode->Data = NewData; //데이터를 저장한다
@@ -10,28 +10,30 @@ Node* DLL_CreateNode(ElementType NewData){
     return NewNode;
 }
 //노드 소멸
-void DLL_DestroyNode(Node* Node){
+void CDLL_DestroyNode(Node* Node){
     free(Node);
 }
 //노드 추가
-void DLL_AppendNode(Node** Head, Node* NewNode){
+void CDLL_AppendNode(Node** Head, Node* NewNode){
     //해드 노드가 NULL이라면 새로운 노드가 Head가 된다.
     if((*Head) == NULL){
         *Head = NewNode;
+        (*Head)->NextMode = *Head;
+        (*Head)->PrevMode = *Head;
     }
     else{
-        //테일을 찾아 NewNode를 연결한다.
-        Node* Tail = (*Head);
-        while (Tail->NextNode != NULL)
-        {
-            Tail = Tail->NextNode;
-        }
+        //테일헤드 사이에 NewNode를 연결한다.
+        Node* Tail = (*Head)->PrevNode;
+        
+        Tail->NextNode->PrevNode = NewNode;
         Tail->NextNode = NewNode;
-        NewNode->PrevNode = Tail; // 기존 테일을 새로운 PreNode가 가리킨다.
+        
+        NewNode->NextNode = (*Head);
+        NewNode->PrevNode = Tail;
     }
 }
 //노드 삽입
-void DLL_InsertAfter(Node* Current, Node* NewNode){
+void CDLL_InsertAfter(Node* Current, Node* NewNode){
     NewNode->NextNode = Current->NextNode;
     NewNode->PrevNode = Current;
 
@@ -44,29 +46,28 @@ void DLL_InsertAfter(Node* Current, Node* NewNode){
 }
 
 //노드 제거
-void DLL_RemoveNode(Node** Head, Node* Remove){
+void CDLL_RemoveNode(Node** Head, Node* Remove){
     if ((*Head) == Remove)
     {
+        (*Head)->PrevNode->NextNode = Remove->NextNode;
+        (*Head)->NextNode->PrevNode = Remove->PrevNode;
+        
         *Head = Remove->NextNode;
+
         Remove->PrevNode = NULL;
         Remove->NextNode = NULL;
     }
     else
     {
-        Node* Temp = Remove;
-        if(Remove->PrevNode != NULL){
-            Remove->PrevNode->NextNode = Temp-> NextNode;
-        }
-        if (Remove->NextNode != NULL)
-        {
-            Remove->NextNode->PrevNode = Temp->PrevNode;
-        }
+        Remove->PrevNode->NextNode = Remove->NextNode;
+        Remove->NextNode->PrevNode = Remove->PrevNode;
+       
         Remove-> PrevNode = NULL;
         Remove-> NextNode = NULL;
     }
 }
 //노드 탐색
-Node* DLL_GetNodeAt(Node* Head, int Location){
+Node* CDLL_GetNodeAt(Node* Head, int Location){
     Node* Current = Head;
 
     while (Current != NULL && (--Location) >= 0)
