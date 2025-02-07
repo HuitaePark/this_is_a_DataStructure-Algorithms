eda1f139f84a23bc209589384e1763c8064cb628
HuitaePark <qkrgmlxo3174@gmail.com>
2025-02-08 02:40:32 +0900

    다익스트라 추가

---
+++ Dijkstra/ShortestPath.h
@@ -0,0 +1,12 @@
+#ifndef SHORTESTPARH_H
+#define SHORTESTPARH_H
+
+#include "../Graph/Graph.h"
+#include "../PriorityQueue/PriorityQueue.h"
+
+#define MAX_WEIGHT 36267
+
+void Dijickstra(Graph* G,Vertex* StartVertex,Graph* MST);
+
+#endif
+
